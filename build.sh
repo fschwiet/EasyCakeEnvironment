000b47ce0d9042ddef432d1879c96f33ee531aca
fschwiet <fschwiet@gmail.com>
2017-12-09 12:22:26 -0500

    environment is setup for cake intellisense

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@
 
 # Define directories.
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-TOOLS_DIR=$SCRIPT_DIR/cake.downloads
+TOOLS_DIR=$SCRIPT_DIR/tools
 ADDINS_DIR=$TOOLS_DIR/Addins
 MODULES_DIR=$TOOLS_DIR/Modules
 NUGET_EXE=$TOOLS_DIR/nuget.exe
